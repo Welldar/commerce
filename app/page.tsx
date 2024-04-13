83ed8946c71c48ae05ad0b74a903874df848ec5b
Welldar <eldarzheryakov@gmail.com>
2024-04-13 22:40:58 +0500

    improv: minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,16 @@ export default async function Page({
   searchParams,
 }: {
   params: { slug: string };
-  searchParams?: { [key: string]: string | string[] };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const productsResponse = await products({ queryArgs: searchParams });
+  const query = new URLSearchParams();
+  for (let [q, value] of Object.entries(searchParams ?? {})) {
+    if (Array.isArray(value)) {
+      value.forEach(v => query.append(q, v));
+    } else query.append(q, value ?? '');
+  }
+
+  const productsResponse = await products({ queryArgs: query });
 
   return <Main slug="" products={productsResponse.results}></Main>;
 }
