996210619d891d6df8f3b9688e88f2bfa6a1a57f
Welldar <eldarzheryakov@gmail.com>
2024-04-15 19:34:52 +0500

    style: add margins

--- app/components/filters.tsx
+++ app/components/filters.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useCallback } from 'react';
+import styles from './filters.module.css';
 
 export function Filters() {
   const router = useRouter();
@@ -19,32 +20,31 @@ export function Filters() {
 
   return (
     <div>
-      <h2>Sort by</h2>
-      <form>
-        <label htmlFor="sort">
-          <select
-            name="sort"
-            id="sort"
-            onChange={e => {
-              // setOptions({ sort: 'lastModifiedAt ' + e.target.value });
-              router.push(
-                pathname +
-                  '?' +
-                  createQueryString(
-                    'sort',
-                    encodeURI('name.en ' + e.target.value)
-                  )
-              );
-            }}
-          >
-            <option value="default" defaultChecked>
-              default
-            </option>
-            <option value="asc">lastModifiedAt asc</option>
-            <option value="desc">lastModifiedAt desc</option>
-          </select>
-        </label>
-      </form>
+      <h3 className={styles.h3}>Sort by</h3>
+      <label htmlFor="sort">
+        <select
+          className={styles.sort}
+          id="sort"
+          onChange={e => {
+            router.push(
+              pathname + '?' + createQueryString('sort', e.target.value)
+            );
+          }}
+        >
+          <option value="lastModifiedAt desc" defaultChecked>
+            newest
+          </option>
+          <option value="name.en-US asc">alphabetic asc</option>
+          <option value="name.en-US  desc">alphabetic desc</option>
+          <option value="variants.scopedPrice.currentValue.centAmount asc">
+            price asc
+          </option>
+          <option value="variants.scopedPrice.currentValue.centAmount desc">
+            price desc
+          </option>
+        </select>
+      </label>
+      <h3 className={styles.h3}>Filter by</h3>
     </div>
   );
 }
