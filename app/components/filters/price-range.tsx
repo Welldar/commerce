86d9b4139c7bf7358e51198dc789997dc3d90332
Welldar <eldarzheryakov@gmail.com>
2024-04-18 12:48:19 +0500

    improv: caret pos consistency

--- app/components/filters/price-range.tsx
+++ app/components/filters/price-range.tsx
@@ -3,45 +3,89 @@
 import { shortFormatter } from '@/app/utility';
 import { useQueryRouting } from '../customHooks';
 import styles from './price-range.module.css';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 export function PriceRange() {
   const queryRouting = useQueryRouting();
 
+  const makeQueryValue = (form: HTMLFormElement) => {
+    const formData = new FormData(form);
+    const parseInput = (field: 'from' | 'to') => {
+      const value = formData.get(field)!.toString().replaceAll(',', '');
+
+      return +value ? value.concat('00') : '*';
+    };
+    const from = parseInput('from');
+    const to = parseInput('to');
+
+    return { from, to };
+  };
+
   return (
     <form
       className={styles['price-range']}
-      onBlur={e => {
-        console.log(e);
-      }}
+      onBlur={e => {}}
       onSubmit={e => {
         e.preventDefault();
-        const form = e.target as HTMLFormElement;
-        const formData = new FormData(form);
-        const from =
-          formData.get('from')?.toString().replaceAll(',', '') ?? '*';
-        const to = formData.get('to')?.toString().replaceAll(',', '') ?? '*';
+
+        const { from, to } = makeQueryValue(e.target as HTMLFormElement);
 
         queryRouting('price_range', `${from}:${to}`);
       }}
-      onInput={e => {
-        if (e.target instanceof HTMLInputElement) {
-          const value = Number(e.target.value.replaceAll(',', ''));
-
-          if (Number.isNaN(value)) return;
-
-          e.target.value = shortFormatter.format(value);
-        }
-      }}
     >
-      <label htmlFor="from" className={styles['price-input']}>
-        <span>from</span>
-        <input id="from" type="text" name="from" inputMode="numeric" />
-      </label>
-      <label htmlFor="to" className={styles['price-input']}>
-        <span>to</span>
-        <input id="to" type="text" name="to" inputMode="numeric" />
-      </label>
+      <PriceInput field="from"></PriceInput>
+      <PriceInput field="to"></PriceInput>
       <button type="submit" style={{ display: 'none' }}></button>
     </form>
+  );
+}
+
+function PriceInput({ field }: { field: string }) {
+  const [value, setValue] = useState('');
+  const [caretPos, setCaretPos] = useState(0);
+  const [trigger, setTrigger] = useState(true);
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    ref.current.selectionStart = ref.current.selectionEnd = caretPos;
+  }, [caretPos, trigger]);
+  return (
+    <label htmlFor={field} className={styles['price-input']}>
+      <span>{field}</span>
+      <input
+        ref={ref}
+        id={field}
+        type="text"
+        name={field}
+        inputMode="numeric"
+        maxLength={9}
+        value={value}
+        onChange={e => {
+          e.preventDefault();
+          const regex = new RegExp(/[^\d,]+/g);
+          const caretPos = e.target.selectionStart ?? 0;
+
+          setCaretPos(caretPos - 1);
+          if (regex.test(e.target.value)) {
+            setTrigger(!trigger);
+            return;
+          }
+
+          const valueNum = +e.target.value.replaceAll(',', '');
+          const length = e.target.value.length;
+
+          if (Number.isNaN(valueNum)) {
+            return;
+          }
+
+          const newValue = shortFormatter.format(valueNum);
+
+          setValue(newValue == '0' ? '' : newValue);
+          setCaretPos(caretPos + newValue.length - length);
+        }}
+      />
+    </label>
   );
 }
