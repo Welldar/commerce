3e2dd3ea6a3e26dd0d2f926a34cf28c95108b459
Welldar <eldarzheryakov@gmail.com>
2024-04-30 15:15:58 +0500

    feat: add loading skeletot for product route

--- app/(routes)/product/[id]/page.tsx
+++ app/(routes)/product/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { Product } from './product';
 import { product as productById } from '@/app/_services/commerce';
-import { parseParams } from '@/app/_utils/utility';
 import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
--- app/(routes)/product/loading.tsx
+++ app/(routes)/product/loading.tsx
@@ -1,5 +1,39 @@
 import Skeleton from '@/app/_components/skeleton';
+import styles from './[id]/product.module.css';
 
 export default function Loading() {
-  return <Skeleton></Skeleton>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.carousel}>
+        <Skeleton style={{ height: '300px' }} />
+      </div>
+      <div>
+        <h2>
+          <Skeleton />
+        </h2>
+        <div>
+          <Skeleton count={5} />
+        </div>
+
+        <div className={styles.attr}>
+          <div className={styles.productSpec} style={{ minWidth: '100px' }}>
+            <Skeleton count={2} />
+          </div>
+          <div style={{ minWidth: '100px' }}>
+            <Skeleton />
+          </div>
+        </div>
+        <div className={styles.variants}>
+          <Skeleton style={{ width: 150, height: 150 }} />
+          <Skeleton style={{ width: 150, height: 150 }} />
+        </div>
+      </div>
+      <div className={styles.checkout}>
+        <div>
+          <Skeleton />
+        </div>
+        <Skeleton />
+      </div>
+    </div>
+  );
 }
